cd67eb1c76fb70926ea8daeecdacce4deae5e567
Gargee <sunf9640@gmail.com>
2026-01-20 11:43:00 +0530

    Completed for and while loop

---
+++ 5_while_loop.py
@@ -0,0 +1,32 @@
+print("Quick Quiz: Write a program to print 1 to 50 using a while loop.")
+
+
+i=0
+while(i<51):
+    print(i)
+    i += 1
+
+    
+
+
+
+print("Quick Quiz: Write a program to print the content of a list using while loops.")
+
+
+l = ["Gargee" ,"Tianxuning" , "Ziyu" ,"Harry" , "Potter" , "Hermioni" , "Granger"]
+
+i =0
+while(i<len(l)):
+    print(l[i])
+    i += 1
+
+    
+
+
+
+i =0
+while(i<20):
+    print("Gargee")
+    i += 1
+
+
---
+++ 6_for_loop.py
@@ -0,0 +1,70 @@
+for i in range(1 , 11):
+    print(i)
+
+
+
+
+
+for i in range(1, 20 , 3):
+    print(i)
+
+print("This will print numbers fro  1 to 19 with the difference of 2")
+
+
+
+
+
+print("for loops in list")
+
+l = [ "Gargee" , 2 , "Tianxuning" , 3 , "ZIyu"]
+
+for i in l:
+    print(i)
+
+
+
+
+
+print("for loops in tuple")
+
+t = ("apple" , 5 , "Banana", 6)
+
+for i in t:
+    print(i)
+
+
+
+
+
+print("for loops in String")
+
+s = "Gargee"
+
+for i in s:
+    print(i)
+
+
+
+
+
+print("break and continue in for loop")
+
+
+for i in range(1,30):
+    if(i==23):
+        break
+    print(i)
+
+for i in range(1,11):
+    if(i==5):
+        continue
+    print(i)
+
+
+
+
+
+print("Pass")
+
+for i in range(100):
+    pass
